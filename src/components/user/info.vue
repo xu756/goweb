3cd0af7b1e6b91016374a6203df9055c23ab802a
xu756 <756334744@qq.com>
2022-05-12 00:03:29 +0800

    更改

--- src/components/user/info.vue
+++ src/components/user/info.vue
@@ -1,8 +1,21 @@
 <template>
-  <el-from>
-    
-  </el-from>
+  <el-from> </el-from>
 </template>
-
 <script>
+export default {
+  data() {
+    return {
+      userinfo: {},
+    };
+  },
+  methods: {
+    getuserinfo() {
+      this.$http
+        .get("userinfo?token=" + localStorage.getItem("token"))
+        .then((res) => {
+          console.log(res.data);
+        });
+    },
+  },
+};
 </script>

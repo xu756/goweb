d7d0fed1cc97998f8762a0ec7dfc1a47d7f6c1d1
xu756 <756334744@qq.com>
2022-05-12 14:01:23 +0800

    结构调整

--- src/components/user/info.vue
+++ src/components/user/info.vue
@@ -1,5 +1,20 @@
 <template>
-  <el-from> </el-from>
+  <div class="UserInfo">
+    <el-form :model="userinfo" label-width="100px">
+      <el-form-item label="用户名" prop="username">
+        <el-input
+          v-model="userinfo.Name"
+          placeholder="请输入你的用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input
+          v-model="userinfo.Email"
+          placeholder="请输入你的邮箱"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
@@ -8,14 +23,23 @@ export default {
       userinfo: {},
     };
   },
+  created() {
+    this.getUserInfo();
+  },
   methods: {
-    getuserinfo() {
+    getUserInfo() {
       this.$http
-        .get("userinfo?token=" + localStorage.getItem("token"))
+        .post("userinfo/user=" + localStorage.getItem("token"))
         .then((res) => {
           console.log(res.data);
+          this.userinfo = res.data.userinfo;
         });
     },
   },
 };
 </script>
+<style lang="scss">
+.UserInfo {
+  width: 20%;
+}
+</style>
